4b58a6e792e901ab976f4083c48364fd6f77ab07
INazca <billy78@gmx.net>
2019-09-05 13:18:00 +0200

    integrated first pdf
    Created a canvas for a pdf to be placed there. The shown pdf is not responsive, which is an issue at the moment. Also the code therefore currently can be found in the index.js, but should be sourced out in future commits

--- app/resources/js/index.js
+++ app/resources/js/index.js
@@ -1,6 +1,34 @@
 /* eslint-env browser */
+var loadingTask,
+    viewport,
+    canvas,
+    context,
+    renderContext;
 
 function init() {
+    console.log("app started!");
+    loadingTask = pdfjsLib.getDocument('./resources/example.pdf');
+    loadingTask.promise.then(function (pdf) {
+        pdf.getPage(1).then(function (page) {
+            var desiredWidth = document.getElementById('reader').getBoundingClientRect().width;
+            console.log(document.getElementById('reader').getBoundingClientRect().width);
+            var viewport = page.getViewport({ scale: 1, });
+            var scale = desiredWidth / viewport.width;
+            viewport = page.getViewport({ scale: scale, });
+            var scaledViewport = page.getViewport({ scale: scale, });
+
+            var canvas = document.getElementById('reader-canvas');
+            var context = canvas.getContext('2d');
+            canvas.height = scaledViewport.height;
+            canvas.width = scaledViewport.width;
+
+            var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+            page.render(renderContext);
+        });
+    });
 }
 
 init();
